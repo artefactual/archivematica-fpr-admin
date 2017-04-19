373642f2f9a7dfe14b95ba974acb5e3e89953fe5
Justin Simpson <jsimpson@artefactual.com>
2017-04-19 15:28:44 -0700

    remove pks from pronom_88 fixture
    setting pk's in this fixture causes loaddata to overwrite existing data.
    
    see #46

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='archivematica-fpr-admin',
-    version='1.6.0rc1',
+    version='1.6.0',
     packages=find_packages(exclude=['testproject']),
     include_package_data=True,
     license='AGPL',
