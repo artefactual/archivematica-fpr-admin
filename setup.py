a02e0c8d5f88dc4ce8f6a51030b0965ba7ec2dbd
Jesús García Crespo <jesus@sevein.com>
2017-06-27 17:46:41 -0700

    Update django-autoslug to v1.9.3
    It solves an incompatibility with Django 1.8. I think that in Django 1.8 the
    prop `model._meta.module_name` was replaced by: `model._meta.model_name`.
    
    This was the error I saw:
    
    ```
    File "/Users/jesus/fpr-admin.env/lib/python2.7/site-packages/autoslug/fields.py", line 221, in pre_save
        slug = instance._meta.module_name
    AttributeError: 'Options' object has no attribute 'module_name'
    ```

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     ],
     install_requires=[
         'django-annoying==0.7.7',
-        'django-autoslug==1.7.1',
+        'django-autoslug==1.9.3',
         'django-extensions==1.1.1',
         'django-forms-bootstrap>=3.0.0,<4.0.0',
     ]
